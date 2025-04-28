c116997941302b2886f255551c02f2c1f257a5b7
Eduardo Paludo <70333794+du-paludo@users.noreply.github.com>
2025-04-28 11:17:10 -0300

    Initial Commit

---
+++ ProjetoTeste/ProjetoTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoTesteApp.swift
+//  ProjetoTeste
+//
+//  Created by Eduardo Paludo on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjetoTesteTests/ProjetoTesteTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProjetoTesteTests.swift
+//  ProjetoTesteTests
+//
+//  Created by Eduardo Paludo on 28/04/25.
+//
+
+import Testing
+
+struct ProjetoTesteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
