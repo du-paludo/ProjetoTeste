45299414ccadaed27a841b8791f3efdebc4d62f2
Eduardo Paludo <70333794+du-paludo@users.noreply.github.com>
2025-04-28 11:27:43 -0300

    texto hello world e imagem alterados

--- ProjetoTeste/ContentView.swift
+++ ProjetoTeste/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "plus")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, people!")
         }
         .padding()
     }
--- ProjetoTeste/ProjetoTesteApp.swift
+++ ProjetoTeste/ProjetoTesteApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ProjetoTesteApp: App {
     var body: some Scene {
         WindowGroup {
+            // ContentView
             ContentView()
         }
     }
